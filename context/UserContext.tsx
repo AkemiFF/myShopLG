35341db1b4a6ecd1acaa547314535acfada089fc
Brad Akemi <mirado.akemi@gmail.com>
2024-11-01 19:02:07 +0300

    context for users

---
+++ context/UserContext.tsx
@@ -0,0 +1,53 @@
+"use client"
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+
+interface User {
+    id: string;
+    username: string;
+    name: string;
+    email: string;
+    isAuthenticated: boolean;
+    first_name: string;
+}
+
+interface UserContextType {
+    user: User | null;
+    setUser: (user: User | null) => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState<User | null>(null);
+
+    // Charger l'utilisateur depuis le localStorage
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+    // Sauvegarder l'utilisateur dans le localStorage
+    useEffect(() => {
+        if (user) {
+            localStorage.setItem('user', JSON.stringify(user));
+        } else {
+            localStorage.removeItem('user');
+        }
+    }, [user]);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export const useUser = (): UserContextType => {
+    const context = useContext(UserContext);
+    if (!context) {
+        throw new Error("useUser doit être utilisé dans un UserProvider");
+    }
+    return context;
+};
