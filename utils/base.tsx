68bba1f56092c79cf38601f173c1d2343fb74a98
Brad Akemi <mirado.akemi@gmail.com>
2024-11-01 19:02:15 +0300

    base fonction

--- utils/base.tsx
+++ utils/base.tsx
@@ -1,4 +1,6 @@
+import { toast } from "react-toastify";
 import { API_BASE_URL } from "./api";
+import getAccessToken from "./cookies";
 
 // utils/api.js (ou dans un fichier de composant)
 export const fetchCategories = async () => {
@@ -17,3 +19,35 @@ export const fetchCategories = async () => {
         return [];
     }
 };
+
+export async function addToCart(productId: any, quantity = 1) {
+    const access = await getAccessToken();
+    try {
+        const response = await fetch(`${API_BASE_URL}api/cart/add/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${access}`,
+            },
+            body: JSON.stringify({ product_id: productId, quantity: quantity }),
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.error || 'Erreur lors de l\'ajout au panier');
+        }
+
+        const data = await response.json();
+        toast.success('Ajout éfféctué !', {
+            theme: "colored",
+            autoClose: 3000,
+        });
+        return data;
+    } catch (error) {
+        console.error('Erreur:', (error as Error).message);
+        toast.error('Une érreur s\'est produite !', {
+            theme: "colored",
+            autoClose: 3000,
+        });
+    }
+}
