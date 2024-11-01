f37db0af9df1f030fdaa7c58d6e93a341db77b52
Brad Akemi <mirado.akemi@gmail.com>
2024-11-01 12:25:37 +0300

    login

--- app/users/login/unforgot/page.tsx
+++ app/users/login/unforgot/page.tsx
@@ -4,18 +4,63 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useAdminLayoutContext } from '@/context/AdminLayoutContext'
+import { API_BASE_URL } from '@/utils/api'
+import { setTokens } from '@/utils/cookies'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import { toast } from 'react-toastify'
 
 export default function LoginPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
+  const router = useRouter();
+  const { userInfo, setUserInfo } = useAdminLayoutContext();
   const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    // Here you would typically handle the login logic
-    console.log('Login attempt with:', { email, password })
-  }
+    e.preventDefault();
+
+    const credentials = {
+      username: email,
+      password: password,
+    };
+
+    fetch(`${API_BASE_URL}api/user/admin/login/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    })
+      .then(response => {
+        if (!response.ok) {
+          toast.error('Mot de passe ou Email incorrecte', {
+            theme: "colored",
+            autoClose: 3000,
+          });
+
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Login successful:', data);
+
+        if (data.is_admin) {
+          setTokens(data.token.access, data.token.refresh);
+          setUserInfo(data.user_info);
+          toast.success('Connexion réussi', {
+            theme: "colored",
+            autoClose: 2000,
+          });
+          setTimeout(() => {
+            router.push('/admin');
+          }, 2000);
+        }
+      })
+      .catch(error => {
+        console.error('Error during login:', error);
+      });
+  };
 
   return (
     <div className="container mx-auto px-4 py-8 flex justify-center items-center min-h-screen">
@@ -30,7 +75,7 @@ export default function LoginPage() {
               <Label htmlFor="email">Email</Label>
               <Input
                 id="email"
-                type="email"
+                type="text"
                 placeholder="votre@email.com"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
