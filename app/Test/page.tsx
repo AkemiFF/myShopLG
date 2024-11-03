7a9beaa776628f09d6331f3021d1430b26e9e216
Brad Akemi <mirado.akemi@gmail.com>
2024-11-03 11:42:20 +0300

    /test

--- app/Test/page.tsx
+++ app/Test/page.tsx
@@ -1,20 +1,10 @@
 "use client"
-import { Button } from "@/components/ui/button";
-import { API_BASE_URL } from "@/utils/api";
 
-const Profile = () => {
-    const testSession = () => {
-        fetch(`${API_BASE_URL}api/cart/session/`, {
-            method: 'POST',
-            credentials: "include",
-        })
-    }
+export default function ProductDetail() {
+
     return (
-        <div>
+        <div className="min-h-screen bg-gray-100">
 
-            <Button onClick={testSession} variant='secondary' >Test session</Button>
         </div>
     );
-};
-
-export default Profile;
+}
