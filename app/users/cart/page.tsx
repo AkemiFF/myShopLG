a77fbf0789879d4c5bb3b0d1d29f62c7fc6281d8
Brad Akemi <mirado.akemi@gmail.com>
2024-11-09 18:37:08 +0300

    users cart

--- app/users/cart/page.tsx
+++ app/users/cart/page.tsx
@@ -1,12 +1,13 @@
 "use client"
+import ProductCard from "@/components/Card/ProductCard"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { useUser } from "@/context/UserContext"
+import { Product } from "@/lib/store"
 import { API_BASE_URL } from "@/utils/api"
 import { addToCart, addToCartOffline } from "@/utils/base"
 import getAccessToken from "@/utils/cookies"
-import { Minus, Plus, Truck, X } from 'lucide-react'
+import { Minus, Plus, X } from 'lucide-react'
 import { useEffect, useState } from "react"
 
 interface CartItem {
@@ -22,6 +23,7 @@ interface CartItem {
 
 export default function CartPage() {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [recommendedProducts, setRecommendedProducts] = useState<Product[]>([]);
   const subtotal = cartItems.reduce((acc, item) => acc + item.total_price, 0)
   const shipping = 5.99
   const total = subtotal + shipping
@@ -29,30 +31,60 @@ export default function CartPage() {
   const { user } = useUser();
 
   const fetchCartItems = async () => {
-    const access = await getAccessToken();
 
-    try {
-      const response = await fetch(`${API_BASE_URL}api/cart/`, {
-        headers: {
-          "Content-Type": "application/json",
-          ...(access ? { "Authorization": `Bearer ${access}` } : {}),
-        },
-        credentials: !access ? 'include' : undefined,
-      });
+    if (user) {
+      const access = await getAccessToken();
+      try {
+        const response = await fetch(`${API_BASE_URL}api/cart/`, {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${access}`
+          },
+        });
 
-      if (response.status === 404) {
-        setCartItems([]);
-        console.log("Votre panier est vide");
-        setNoCart(true);
-      } else if (!response.ok) {
-        throw new Error("Erreur lors de la récupération des articles du panier");
-      } else {
-        const data = await response.json();
-        setCartItems(data.items);
+        if (response.status === 404) {
+          setCartItems([]);
+          setNoCart(true);
+        } else if (!response.ok) {
+          throw new Error("Erreur lors de la récupération des articles du panier");
+        } else {
+          const data = await response.json();
+
+          setRecommendedProducts(data.recommended_products)
+          setCartItems(data.items);
+        }
+      } catch (error) {
+        console.error("Erreur:", error);
       }
-    } catch (error) {
-      // console.error("Erreur:", error);
+    } else {
+
+      try {
+        const response = await fetch(`${API_BASE_URL}api/cart/`, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: 'include',
+        });
+
+        if (response.status === 404) {
+          setCartItems([]);
+          setNoCart(true);
+        } else if (!response.ok) {
+          // throw new Error("Erreur lors de la récupération des articles du panier");
+          return;
+        } else {
+          const data = await response.json();
+          setRecommendedProducts(data.recommended_products)
+          setCartItems(data.items);
+          if (data.items.length === 0) {
+            setNoCart(true);
+          }
+
+        }
+      } catch (error) {
+      }
     }
+
   };
 
 
@@ -187,42 +219,48 @@ export default function CartPage() {
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           <div className="lg:col-span-2">
-            <Card>
-              <CardHeader>
-                <CardTitle>Articles dans votre panier</CardTitle>
-              </CardHeader>
-              <CardContent>
-                {cartItems.map((item) => (
-                  <div key={item.product_id} className="flex items-center space-x-4 py-4 border-b last:border-b-0">
-                    <img src={item.image_url ? API_BASE_URL + item.image_url : "/placeholder.svg"} alt={item.product_name} className="w-24 h-24 object-cover rounded" />
-                    <div className="flex-1">
-                      <h3 className="font-semibold">{item.product_name}</h3>
-                      <p className="text-gray-600">{item.price.toFixed(2)} €</p>
-                      <div className="flex items-center space-x-2 mt-2">
-                        <Button onClick={() => handleLessProduct(item.product_id)} variant="outline" size="icon">
-                          <Minus className="h-4 w-4" />
-                        </Button>
-                        <span>{item.quantity}</span>
+            {noCart ? null : (
 
-                        <Button onClick={() => handleMoreProduct(item.product_id)} variant="outline" size="icon">
-                          <Plus className="h-4 w-4" />
-                        </Button>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Articles dans votre panier</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  {cartItems.map((item) => (
+                    <div key={item.product_id} className="flex items-center space-x-4 py-4 border-b last:border-b-0">
+                      <img src={item.image_url ? API_BASE_URL + item.image_url : "/placeholder.svg"} alt={item.product_name} className="w-24 h-24 object-cover rounded" />
+                      <div className="flex-1">
+                        <h3 className="font-semibold">{item.product_name}</h3>
+                        <p className="text-gray-600">{item.price.toFixed(2)} €</p>
+                        <div className="flex items-center space-x-2 mt-2">
+                          <Button onClick={() => handleLessProduct(item.product_id)} variant="outline" size="icon">
+                            <Minus className="h-4 w-4" />
+                          </Button>
+                          <span>{item.quantity}</span>
+
+                          <Button onClick={() => handleMoreProduct(item.product_id)} variant="outline" size="icon">
+                            <Plus className="h-4 w-4" />
+                          </Button>
+                        </div>
                       </div>
+                      <Button onClick={() => handleRemoveItem(item.product_id)} variant="ghost" size="icon">
+                        <X className="h-4 w-4" />
+                      </Button>
                     </div>
-                    <Button onClick={() => handleRemoveItem(item.product_id)} variant="ghost" size="icon">
-                      <X className="h-4 w-4" />
-                    </Button>
-                  </div>
-                ))}
-              </CardContent>
-            </Card>
+                  ))}
+                </CardContent>
+              </Card>
+            )
+            }
           </div>
 
           <div>
             {noCart ? (
-              <>
-                Votre panier est vide
-              </>
+              <Card>
+                <CardHeader>
+                  <CardTitle> Votre panier est vide</CardTitle>
+                </CardHeader>
+              </Card>
             ) : (<><Card>
               <CardHeader>
                 <CardTitle>Résumé de la commande</CardTitle>
@@ -246,53 +284,25 @@ export default function CartPage() {
                   <Button className="w-full mt-4">Procéder au paiement</Button>
                 </a>
               </CardContent>
-            </Card><Card className="mt-4">
-                <CardHeader>
-                  <CardTitle>Options de livraison</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <Select>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Choisir une option de livraison" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="standard">
-                        <div className="flex items-center">
-                          <Truck className="mr-2 h-4 w-4" />
-                          <span>Livraison standard (3-5 jours)</span>
-                        </div>
-                      </SelectItem>
-                      <SelectItem value="express">
-                        <div className="flex items-center">
-                          <Truck className="mr-2 h-4 w-4" />
-                          <span>Livraison express (1-2 jours)</span>
-                        </div>
-                      </SelectItem>
-                    </SelectContent>
-                  </Select>
-                </CardContent>
-              </Card>
+            </Card>
+
+
             </>
             )}
 
           </div>
         </div>
-
-        <div className="mt-12">
-          <h2 className="text-2xl font-bold mb-4">Recommandations</h2>
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-            {[1, 2, 3, 4].map((product) => (
-              <Card key={product}>
-                <CardContent className="p-4">
-                  <img src={`/placeholder.svg?height=150&width=150`} alt={`Produit recommandé ${product}`} className="w-full h-auto mb-2" />
-                  <h3 className="font-semibold">Produit Recommandé {product}</h3>
-                  <p className="text-gray-600 mb-2">XX,XX €</p>
-                  <Button variant="outline" size="sm" className="w-full">Ajouter au panier</Button>
-                </CardContent>
-              </Card>
-            ))}
+        {noCart ? null : (
+          <div className="mt-12">
+            <h2 className="text-2xl font-bold mb-4">Recommandations</h2>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+              {recommendedProducts.map((item) => (
+                <ProductCard key={item.id} product={item} />
+              ))}
+            </div>
           </div>
-        </div>
+        )
+        }
       </main>
     </div>
   )
