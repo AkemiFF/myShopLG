1f2fa8fc17b5f35adcd582b89d05cb80b3fe4dc0
Brad Akemi <mirado.akemi@gmail.com>
2024-11-09 18:36:20 +0300

    admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -9,9 +9,9 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
 
       {/* Main content */}
       <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
-        <div className="container mx-auto px-6 py-8">
-          {children}
-        </div>
+        {/* <div className="container mx-auto px-6 py-8"> */}
+        {children}
+        {/* </div> */}
       </main>
     </div>
   )
