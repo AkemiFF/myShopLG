af9d415836aab9865d1135943cdd7d51b84c0751
Brad Akemi <mirado.akemi@gmail.com>
2024-11-10 18:04:17 +0300

    middelware

--- middleware.jsx
+++ middleware.jsx
@@ -3,13 +3,25 @@ import { NextResponse } from 'next/server';
 // Middleware pour vérifier la présence du cookie `refresh_token_main`
 export function middleware(request) {
     const admin_token = request.cookies.get('refresh_token_main');
+    const client_token = request.cookies.get('refresh_token');
     const url = request.nextUrl.clone();
-
-    console.log("hey");
 
     if (url.pathname.startsWith('/users/login/unforgot')) {
         if (admin_token) {
             return NextResponse.redirect(new URL('/admin', request.url));
+        }
+        return NextResponse.next();
+    }
+
+    // if (url.pathname.startsWith('/users/profil')) {
+    //     if (!client_token) {
+    //         return NextResponse.redirect(new URL('/404', request.url));
+    //     }
+    //     return NextResponse.next();
+    // }
+    if (url.pathname.startsWith('/users/login') && !url.pathname.startsWith('/users/login/unforgot') && !url.pathname.startsWith('/users/login/forgot-password')) {
+        if (client_token) {
+            return NextResponse.redirect(new URL('/users', request.url));
         }
         return NextResponse.next();
     }
@@ -21,16 +33,10 @@ export function middleware(request) {
         }
         return NextResponse.next();
     }
-    // if (request.nextUrl.pathname.startsWith('/admin')) {
-    //     const refreshToken = request.cookies.get('refresh_token_main');
-    //     if (!refreshToken) {
-    //         return NextResponse.redirect(new URL('/users', request.url));
-    //     }
-    // }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/admin/:path*'], // Appliquer à toutes les routes sous /admin
+    matcher: ['/admin/:path*', '/users/:path*'],
 };
