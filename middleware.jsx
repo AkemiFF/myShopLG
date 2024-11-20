bb9b27cb2d284d9d72025c78b24fb9af9feb1ba6
Brad Akemi <mirado.akemi@gmail.com>
2024-11-20 15:30:44 +0300

    middleware

--- middleware.jsx
+++ middleware.jsx
@@ -3,6 +3,7 @@ import { NextResponse } from 'next/server';
 // Middleware pour vérifier la présence du cookie `refresh_token_main`
 export function middleware(request) {
     const admin_token = request.cookies.get('refresh_token_main');
+    const manager_token = request.cookies.get('refresh_token_manager');
     const client_token = request.cookies.get('refresh_token');
     const url = request.nextUrl.clone();
 
@@ -13,12 +14,13 @@ export function middleware(request) {
         return NextResponse.next();
     }
 
-    // if (url.pathname.startsWith('/users/profil')) {
-    //     if (!client_token) {
-    //         return NextResponse.redirect(new URL('/404', request.url));
-    //     }
-    //     return NextResponse.next();
-    // }
+    if (url.pathname.startsWith('/users/login/unforgot')) {
+        if (manager_token) {
+            return NextResponse.redirect(new URL('/manager', request.url));
+        }
+        return NextResponse.next();
+    }
+
     if (url.pathname.startsWith('/users/login') && !url.pathname.startsWith('/users/login/unforgot') && !url.pathname.startsWith('/users/login/forgot-password')) {
         if (client_token) {
             return NextResponse.redirect(new URL('/users', request.url));
