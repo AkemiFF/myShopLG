025c78f36d0651c358d391ec69d21ca559ad7fa5
Brad Akemi <mirado.akemi@gmail.com>
2024-11-20 15:32:27 +0300

    cookies

--- utils/cookies.tsx
+++ utils/cookies.tsx
@@ -30,6 +30,20 @@ export const setAdminTokens = (accessToken: string, refreshToken: string) => {
     });
 };
 
+export const setManagerTokens = (accessToken: string, refreshToken: string) => {
+    // Set tokens with an expiration time
+    Cookies.set('access_token_manager', accessToken, {
+        expires: 1 / 48,
+        secure: true,
+        sameSite: 'Lax',
+    });
+    Cookies.set('refresh_token_manager', refreshToken, {
+        expires: 7,
+        secure: true,
+        sameSite: 'Lax',
+    });
+};
+
 
 export const getTokens = () => {
     const accessToken = Cookies.get('access_token');
@@ -113,6 +127,44 @@ export const getAdminAccessToken = async () => {
         } catch (error) {
             // console.error('Error refreshing access token:', error);
             return null;
+        }
+    }
+
+    return accessToken;
+};
+export const getManagerAccessToken = async () => {
+    let accessToken = Cookies.get('access_token_manager');
+
+    if (!accessToken) {
+        try {
+            const response = await fetch(`${API_BASE_URL}api/token/refresh/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    refresh: Cookies.get('refresh_token_manager'),
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to refresh access token');
+            }
+
+            const data = await response.json();
+            accessToken = data.access;
+
+            // Set the new access token in cookies
+            if (accessToken) {
+                Cookies.set('access_token_manager', accessToken, {
+                    expires: 1 / 24,
+                    secure: true,
+                    sameSite: 'Lax',
+                });
+            }
+        } catch (error) {
+            // console.error('Error refreshing access token:', error);
+            return undefined;
         }
     }
 
