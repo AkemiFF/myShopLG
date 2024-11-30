48a3ea1af47cb6a4ce3d7f788d3b3c21200c0fba
Brad Akemi <mirado.akemi@gmail.com>
2024-11-30 20:47:54 +0300

    test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,21 @@
 'use client'
 
 import { Button } from '@/components/ui/button';
+import { payCheck } from '@/utils/payCheck';
 import { initiateCartPayment } from '@/utils/payments';
+import { useEffect } from 'react';
 
 export default function LoginPage() {
     const handlePay = async () => {
         const data = await initiateCartPayment(1);
         console.log(data);
     }
+    useEffect(() => {
+        const res = payCheck('REF4835T150P0')
+        console.log(Promise.resolve(res));
+
+    }, []);
+
     return (
         <div className="container mx-auto px-4 py-8 flex justify-center items-center min-h-screen">
             <Button onClick={handlePay} name='Home' />
