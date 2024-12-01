05670bc8a2bbef9548bf8418421f56be6652eb0a
Brad Akemi <mirado.akemi@gmail.com>
2024-12-01 08:59:27 +0300

    order confirmation

---
+++ app/users/cart/order-confirmation/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { payCheck } from '@/utils/payCheck'
+import { useEffect, useState } from 'react'
+import LoadingSpinner from './LoadingSpinner'
+import OrderStatus from './OrderStatus'
+
+export default function OrderConfirmation() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isOrderAccepted, setIsOrderAccepted] = useState<boolean | null>(null)
+
+  useEffect(() => {
+    const ref = localStorage.getItem("reference_order");
+    console.log(ref);
+
+    const verifyOrder = async (ref: string) => {
+      const check = await payCheck(ref);
+      console.log(check);
+
+      if (check) {
+        setIsOrderAccepted(true);
+        setIsLoading(false);
+      } else {
+        setIsOrderAccepted(false)
+        setIsLoading(false);
+      }
+    }
+    if (ref) {
+      verifyOrder(ref);
+    }
+
+  }, [])
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <LoadingSpinner visible={isLoading} />
+      {!isLoading && <OrderStatus isAccepted={isOrderAccepted} />}
+    </div>
+  )
+}
+
