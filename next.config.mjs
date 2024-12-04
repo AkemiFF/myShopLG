9e1988d41bb96dda132c1137f59c4c3836ae13c8
Brad Akemi <mirado.akemi@gmail.com>
2024-12-04 14:35:00 +0300

    origin edit

--- next.config.mjs
+++ next.config.mjs
@@ -43,20 +43,13 @@ const nextConfig = {
                         key: "Content-Security-Policy",
                         value: `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src 'self' ${process.env.NEXT_PUBLIC_BACKEND_URL}; img-src 'self' https: data: blob:;  font-src 'self' data:;`
                     },
-                    {
-                        key: "Access-Control-Allow-Origin",
-                        value: process.env.NEXT_PUBLIC_BACKEND_URL
-                    },
-
+                    { key: "Access-Control-Allow-Origin", value: "https://shoplg.online" },
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     {
                         key: "Access-Control-Allow-Methods",
                         value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"
                     },
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-                    },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ],
             },
         ];
