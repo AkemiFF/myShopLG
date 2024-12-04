8f1b9edc6bd995d18bc145d4e81ce9f08c869942
Brad Akemi <mirado.akemi@gmail.com>
2024-12-04 07:45:30 +0300

    config

--- next.config.mjs
+++ next.config.mjs
@@ -19,15 +19,44 @@ const nextConfig = {
     },
 
     async headers() {
+        // return [
+        //     {
+        //         source: "/(.*)",
+        //         headers: [
+        //             { key: "X-Frame-Options", value: "DENY" },
+        //             { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains; preload" },
+        //             { key: "X-Content-Type-Options", value: "nosniff" },
+        //             { key: "Referrer-Policy", value: "no-referrer" },
+        //             { key: "Content-Security-Policy", value: "script-src 'self' 'unsafe-inline' 'unsafe-eval';" },
+        //         ],
+        //     },
+        // ];
         return [
             {
                 source: "/(.*)",
                 headers: [
-                    { key: "X-Frame-Options", value: "DENY" },
+                    { key: "X-Frame-Options", value: "SAMEORIGIN" },
                     { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains; preload" },
                     { key: "X-Content-Type-Options", value: "nosniff" },
-                    { key: "Referrer-Policy", value: "no-referrer" },
-                    { key: "Content-Security-Policy", value: "script-src 'self' 'unsafe-inline' 'unsafe-eval';" },
+                    { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+                    {
+                        key: "Content-Security-Policy",
+                        value: `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src 'self' ${process.env.NEXT_PUBLIC_BACKEND_URL}; img-src 'self' https: data:; font-src 'self' data:;`
+                    },
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: process.env.NEXT_PUBLIC_BACKEND_URL
+                    },
+
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+                    },
                 ],
             },
         ];
