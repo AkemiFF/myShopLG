f676a6894e6ce2b636ce0ae0bcd020eb942c8fe5
Brad Akemi <mirado.akemi@gmail.com>
2025-07-13 13:06:54 +0300

    conf

--- next.config.mjs
+++ next.config.mjs
@@ -14,48 +14,39 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'gkjguutd-shop-chjv-ugu.onrender.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'gkjguutd-shop-chjv-ugu-05uj.onrender.com',
+            },
         ],
         minimumCacheTTL: 60,
     },
 
     async headers() {
-        // return [
-        //     {
-        //         source: "/(.*)",
-        //         headers: [
-        //             { key: "X-Frame-Options", value: "DENY" },
-        //             { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains; preload" },
-        //             { key: "X-Content-Type-Options", value: "nosniff" },
-        //             { key: "Referrer-Policy", value: "no-referrer" },
-        //             { key: "Content-Security-Policy", value: "script-src 'self' 'unsafe-inline' 'unsafe-eval';" },
-        //         ],
-        //     },
-        // ];
         return [
             {
                 source: "/(.*)",
-
                 headers: [
-                    { key: "X-Frame-Options", value: "SAMEORIGIN" },
+                    { key: "X-Frame-Options", value: "DENY" },
                     { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains; preload" },
                     { key: "X-Content-Type-Options", value: "nosniff" },
-                    { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+                    { key: "Referrer-Policy", value: "no-referrer" },
                     {
                         key: "Content-Security-Policy",
-                        value: `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src 'self' https://api.shoplg.online; img-src 'self' https: data: blob:;  font-src 'self' data:;`
+                        value: `
+                            default-src 'self';
+                            script-src 'self' 'unsafe-inline' 'unsafe-eval';
+                            style-src 'self' 'unsafe-inline';
+                            img-src 'self' https://giphy.com https://i.giphy.com https://gkjguutd-shop-chjv-ugu-05uj.onrender.com;
+                            connect-src 'self' https://gkjguutd-shop-chjv-ugu-05uj.onrender.com;
+                            frame-ancestors 'none';
+                        `.replace(/\n/g, " ")
                     },
-                    { key: "Access-Control-Allow-Origin", value: "https://api.shoplg.online" },
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-                    },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
                 ],
             },
         ];
     },
-    poweredByHeader: false, // Supprimer l'en-tête X-Powered-By
+    poweredByHeader: false,
 };
 
 export default nextConfig;
