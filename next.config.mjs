9362a96726ccabfa2e22a47c1e92e32cff51ca8c
Brad Akemi <mirado.akemi@gmail.com>
2025-07-13 16:28:15 +0300

    config local

--- next.config.mjs
+++ next.config.mjs
@@ -25,24 +25,24 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/(.*)",
-                headers: [
-                    { key: "X-Frame-Options", value: "DENY" },
-                    { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains; preload" },
-                    { key: "X-Content-Type-Options", value: "nosniff" },
-                    { key: "Referrer-Policy", value: "no-referrer" },
-                    {
-                        key: "Content-Security-Policy",
-                        value: `
-                            default-src 'self';
-                            script-src 'self' 'unsafe-inline' 'unsafe-eval';
-                            style-src 'self' 'unsafe-inline';
-                            img-src 'self' https://giphy.com https://i.giphy.com https://gkjguutd-shop-chjv-ugu-05uj.onrender.com;
-                            connect-src 'self' https://gkjguutd-shop-chjv-ugu-05uj.onrender.com;
-                            frame-ancestors 'none';
-                        `.replace(/\n/g, " ")
-                    },
-                ],
+            source: "/(.*)",
+            headers: [
+                { key: "X-Frame-Options", value: "DENY" },
+                { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains; preload" },
+                { key: "X-Content-Type-Options", value: "nosniff" },
+                { key: "Referrer-Policy", value: "no-referrer" },
+                {
+                key: "Content-Security-Policy",
+                value: `
+                    default-src 'self';
+                    script-src 'self' 'unsafe-inline' 'unsafe-eval';
+                    style-src 'self' 'unsafe-inline';
+                    img-src 'self' https://giphy.com https://i.giphy.com https://gkjguutd-shop-chjv-ugu-05uj.onrender.com http://localhost:8000;
+                    connect-src 'self' https://gkjguutd-shop-chjv-ugu-05uj.onrender.com http://localhost:8000 http://127.0.0.1:8000;
+                    frame-ancestors 'none';
+                `.replace(/\n/g, " ")
+                },
+            ],
             },
         ];
     },
